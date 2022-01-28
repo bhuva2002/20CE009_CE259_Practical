04adf5d5ae762e28cf9243a683ee6fe94ada5089
bhuva2002 <77018521+bhuva2002@users.noreply.github.com>
2022-01-28 21:59:35 +0530

    Create Set_c.py

---
+++ Practical2/Set/Set_c.py
@@ -0,0 +1,16 @@
+# AIM: Write a Python program to create an intersection, Union, difference of sets.
+# Name: Yash Bhuva 
+# Id: 20CE009 
+# Github Repository Link: https://github.com/bhuva2002/20CE009_CE259_Practical
+
+A = {0, 2, 4, 6, 8}
+B = {1, 2, 3, 4, 5}
+  
+# union
+print("Union :", A | B)
+  
+# intersection
+print("Intersection :", A & B)
+
+# difference
+print("Difference :", A - B)
