4640abeadfc8f8b11b55411b20794f7c054c9c71
bhuva2002 <77018521+bhuva2002@users.noreply.github.com>
2022-01-28 21:56:35 +0530

    Create Tuple_b.py

---
+++ Practical2/Tuple/Tuple_b.py
@@ -0,0 +1,8 @@
+# AIM: Write a Python program to create a tuple with numbers and print one item.
+# Name: Yash Bhuva 
+# Id: 20CE009 
+# Github Repository Link: https://github.com/bhuva2002/20CE009_CE259_Practical
+
+a = 23, 34, 56
+print(type(a))
+print(a[0])
