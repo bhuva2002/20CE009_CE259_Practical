07a912a23730b1ec4e88557c872648d936b9081f
bhuva2002 <77018521+bhuva2002@users.noreply.github.com>
2022-01-28 22:00:09 +0530

    Create Set_d.py

---
+++ Practical2/Set/Set_d.py
@@ -0,0 +1,8 @@
+# AIM: Write a Python program to find maximum and the minimum value in a set.
+# Name: Yash Bhuva 
+# Id: 20CE009 
+# Github Repository Link: https://github.com/bhuva2002/20CE009_CE259_Practical
+
+set1 = set([8, 7,19, 46, 50, 3])
+print('Maximum element of set is ', max(set1))
+print('Minimum element of set is ', min(set1))
