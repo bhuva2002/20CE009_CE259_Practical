58f01685f3c09920ea157966d351d4102e2643b9
bhuva2002 <77018521+bhuva2002@users.noreply.github.com>
2022-01-28 22:00:49 +0530

    Create Set_e.py

---
+++ Practical2/Set/Set_e.py
@@ -0,0 +1,64 @@
+# AIM: Write a Python program to find the most common elements and their counts from list, tuple, dictionary.
+# Name: Yash Bhuva 
+# Id: 20CE009 
+# Github Repository Link: https://github.com/bhuva2002/20CE009_CE259_Practical
+
+def most_CommonList(List):
+    counter = 0
+    num = List[0]
+
+    for i in List:
+        curr_frequency = List.count(i)
+        if(curr_frequency > counter):
+            counter = curr_frequency
+            num = i
+
+    return num, counter
+
+
+List = [2, 1, 2, 2, 2, 1, 3]
+ans = most_CommonList(List)
+print(f'Your most common Elements in list is {ans[0]} and this is repeat {ans[1]} times.')
+
+# element in a tuple
+
+def commonElement_tuple(List):
+    counter = 0
+    num = List[0]
+
+    for i in List:
+        curr_frequency = List.count(i)
+        if(curr_frequency > counter):
+            counter = curr_frequency
+            num = i
+
+    return num, counter
+
+
+List = (3, 4, 5, 4, 3, 2, 1, 2, 3)
+ans1 = commonElement_tuple(List)
+print(f'Your most common Elements in tuple is {ans1[0]} and this is repeat {ans1[1]} times.')
+
+
+# most common elements in dictionary
+a = {'a': 1, 'b': 2, 'c': 1, 'd': 2, 'e': 2}
+lis = []
+for i in a.values():
+    lis.append(i)
+
+
+def most_CommonDictionary(List):
+    counter = 0
+    num = List[0]
+
+    for j in List:
+        curr_frequency = List.count(j)
+        if(curr_frequency > counter):
+            counter = curr_frequency
+            num = j
+
+    return num, counter
+
+
+ans2 = most_CommonDictionary(lis)
+print(f'Your most common Elements in Dicionary is {ans2[0]} and this is repeat {ans2[1]} times.')
