28fd58bd11a2ebaa15888d91f541e8b1c65e5781
bhuva2002 <77018521+bhuva2002@users.noreply.github.com>
2022-01-28 21:57:20 +0530

    Create Tuple_d.py

---
+++ Practical2/Tuple/Tuple_d.py
@@ -0,0 +1,10 @@
+# AIM: Write a Python program to convert a tuple to a string.
+# Name: Yash Bhuva 
+# Id: 20CE009 
+# Github Repository Link: https://github.com/bhuva2002/20CE009_CE259_Practical
+
+a = ('a', 'b', 'c', 'd')
+print(type(a))
+b = ''.join(a)
+print(b)
+print(type(b))
