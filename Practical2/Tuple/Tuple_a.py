c2651620ad6a2527a0a447445e51dc45607ffd85
bhuva2002 <77018521+bhuva2002@users.noreply.github.com>
2022-01-28 21:56:05 +0530

    Create Tuple_a.py

---
+++ Practical2/Tuple/Tuple_a.py
@@ -0,0 +1,8 @@
+# AIM: Write a Python program to create a tuple with different data types.
+# Name: Yash Bhuva 
+# Id: 20CE009 
+# Github Repository Link: https://github.com/bhuva2002/20CE009_CE259_Practical
+
+a = ('Hello', 34, True, 46.78)
+print(type(a)) 
+print(a)
