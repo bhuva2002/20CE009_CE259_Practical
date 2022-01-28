e589fb7f56615ec0dbc5a7ddaa25feafbfbcfdad
bhuva2002 <77018521+bhuva2002@users.noreply.github.com>
2022-01-28 21:57:42 +0530

    Create Tuple_e.py

---
+++ Practical2/Tuple/Tuple_e.py
@@ -0,0 +1,7 @@
+# AIM: Write a Python program to find the length of a tuple.
+# Name: Yash Bhuva 
+# Id: 20CE009 
+# Github Repository Link: https://github.com/bhuva2002/20CE009_CE259_Practical
+
+a = ('a', 'b', 'c', 'd', 12, True, 45.90)
+print(len(a))
