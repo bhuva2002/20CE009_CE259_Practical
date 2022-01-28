aa4991b46fb3865b3625ae00493e2abf7117c65b
bhuva2002 <77018521+bhuva2002@users.noreply.github.com>
2022-01-28 22:06:32 +0530

    Update Practical3.py

--- Practical3.py
+++ Practical3.py
@@ -1,1 +1,16 @@
+# AIM: Find Captain Room Number
+# Name: Yash Bhuva 
+# Id: 20CE009 
+# Github Repository Link: https://github.com/bhuva2002/20CE009_CE259_Practical
 
+n = input() #total room
+lst = []
+
+lst = [int(RoomNo) for RoomNo in input().split()] #room no for each player
+
+length = len(lst)
+for i in range(length): #traverse in list
+    if lst.count(lst[i])==1: 
+        print(lst[i])
+        exit
+        
