1db273e578dd84e1690f42699db9a00fbb69517c
bhuva2002 <77018521+bhuva2002@users.noreply.github.com>
2022-01-28 21:54:40 +0530

    Create Dic_d.py

---
+++ Practical2/Dictionary/Dic_d.py
@@ -0,0 +1,11 @@
+# AIM: Write a Python script to add a key to a dictionary. 
+# Sample Dictionary : {0: 10, 1: 20} 
+# Expected Result : {0: 10, 1: 20, 2: 30}
+
+# Name: Yash Bhuva 
+# Id: 20CE009 
+# Github Repository Link: https://github.com/bhuva2002/20CE009_CE259_Practical
+
+d = {0:10, 1:20}
+d.update({2:30})
+print(d)
