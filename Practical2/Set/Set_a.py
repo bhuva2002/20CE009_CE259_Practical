48cb6809ae883864d38c196c885b935264bc4c7f
bhuva2002 <77018521+bhuva2002@users.noreply.github.com>
2022-01-28 21:58:36 +0530

    Create Set_a.py

---
+++ Practical2/Set/Set_a.py
@@ -0,0 +1,11 @@
+# AIM: Write a Python program to add member(s) in a set and clear a set
+# Name: Yash Bhuva 
+# Id: 20CE009 
+# Github Repository Link: https://github.com/bhuva2002/20CE009_CE259_Practical
+
+a = set([1,2])  
+print('first', a)
+a.update([3,4])
+print('second', a)
+a.clear()
+print(a)
