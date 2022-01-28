245b123b6f44d07c2dcc21c0a8bb2856495335b9
bhuva2002 <77018521+bhuva2002@users.noreply.github.com>
2022-01-28 21:56:57 +0530

    Create Tuple_c.py

---
+++ Practical2/Tuple/Tuple_c.py
@@ -0,0 +1,9 @@
+# AIM: Write a Python program to add an item in a tuple.
+# Name: Yash Bhuva 
+# Id: 20CE009 
+# Github Repository Link: https://github.com/bhuva2002/20CE009_CE259_Practical
+
+a = (4, 6, 2, 8, 3, 1) 
+print(a)
+a = a + (9,)
+print(a)
